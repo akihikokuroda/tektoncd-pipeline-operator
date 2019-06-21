1a2fef2ee30e2d8b255eff27e193aaaab2a99f17
akihikokuroda <akuroda@us.ibm.com>
2019-06-21 10:07:45 -0400

    add ingress

--- addons/dashboard/install_pipeline_w_dashboard.sh
+++ addons/dashboard/install_pipeline_w_dashboard.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 cd addons/dashboard
-kubectl apply -f dashboard.yaml
-cat ../../deploy/service_account.yaml divider.txt ../../deploy/role.yaml divider.txt ../../deploy/role_binding.yaml divider.txt ../../deploy/operator.yaml mount.txt > namespaced.yaml
+sed -e s/\<dashboard-host\>/$1/g dashboard.yaml > dashboard1.yaml
+kubectl apply -f dashboard1.yaml
+rm dashboard1.yaml
+sed ':a;N;$!ba;s/resources:\n  - deployments\n  verbs:/resources:\n  - deployments\n  - ingresses\n  verbs:/g' ../../deploy/role.yaml > role.yaml
+cat ../../deploy/service_account.yaml divider.txt role.yaml divider.txt ../../deploy/role_binding.yaml divider.txt ../../deploy/operator.yaml mount.txt > namespaced.yaml
+rm role.yaml
 cd -
 kubectl apply  -f  addons/dashboard/namespaced.yaml
 cd addons/dashboard

1c6d46c1d75b89baaa68491c45f35ecd3368661c
akihikokuroda <akuroda@us.ibm.com>
2019-07-01 13:52:50 -0400

    update clusterrole

--- addons/webhook/install_pipeline_w_webhook.sh
+++ addons/webhook/install_pipeline_w_webhook.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd addons/webhook
 kubectl apply -f webhook.yaml
-cat ../../deploy/service_account.yaml divider.txt ../../deploy/role.yaml divider.txt ../../deploy/role_binding.yaml divider.txt ../../deploy/operator.yaml mount.txt > namespaced.yaml
+cat ../../deploy/service_account.yaml divider.txt ../../deploy/role.yaml additionalrole.txt divider.txt ../../deploy/role_binding.yaml divider.txt ../../deploy/operator.yaml mount.txt > namespaced.yaml
 cd -
 kubectl apply  -f  addons/webhook/namespaced.yaml
 cd addons/webhook
